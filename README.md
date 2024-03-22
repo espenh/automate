38311af6eb5b652026f8e51920f6b0dd91101859
Espen Halvorsen <espen.halvorsen@gmail.com>
2024-03-22 09:57:42 +0100

    Start on automate

---
+++ README.md
@@ -0,0 +1,19 @@
+# automate, by consid
+
+A car bot.
+
+## Development
+
+Create a `.env` file based on `.env.example`.
+
+To ingest the files into pinecone:
+```sh
+npm run ingest
+```
+
+To start the dev server:
+```sh
+npm run dev
+```
+
+Then open http://localhost:3000/.
---
+++ scripts/ingestData.ts
@@ -0,0 +1,67 @@
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { getDocument, version } from "pdfjs-dist";
+import { Chroma } from "@langchain/community/vectorstores/chroma"
+import fs from "fs";
+
+// This script ingests all pdfs in the docs folder into Pinecone.
+
+const docsFolder = 'docs';
+
+export const run = async () => {
+  try {
+    // Check that the docs folder exists.
+    const fullDocsFolderPath = `${process.cwd()}/${docsFolder}`;
+    if (!fs.existsSync(fullDocsFolderPath)) {
+      throw new Error('The directory does not exist: ' + fullDocsFolderPath);
+    }
+
+    // Load all pdfs in the docs folder.
+    const directoryLoader = new DirectoryLoader(docsFolder, {
+      '.pdf': (path) => new PDFLoader(path, {
+        pdfjs: async () => {
+          return {
+            getDocument, version
+          }
+        }
+      }),
+    });
+
+    console.log("Loading pdfs")
+    const rawDocs = await directoryLoader.load();
+
+    /* Split text into chunks */
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 2000,
+      chunkOverlap: 400,
+    });
+
+    console.log("Splitting documents")
+    const docs = await textSplitter.splitDocuments(rawDocs);
+
+    const embeddings = new OpenAIEmbeddings({
+      modelName: 'text-embedding-3-large',
+      dimensions: 1024,
+    });
+
+    console.log("Ingesting documents into Pinecone")
+    await Chroma.fromDocuments(docs, embeddings, {
+      collectionName: "a-test-collection",
+      url: "http://localhost:8000",
+      collectionMetadata: {
+        "hnsw:space": "cosine" // https://docs.trychroma.com/usage-guide#changing-the-distance-function
+      }
+    });
+
+  } catch (error) {
+    console.log('error', error);
+    throw new Error('Failed to ingest your data');
+  }
+};
+
+(async () => {
+  await run();
+  console.log('Ingestion complete');
+})();
